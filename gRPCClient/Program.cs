84a6266cf2881330e04f3885707771dc7517d0f7
daihb <daihb@DESKTOP-R31MQAB>
2019-06-24 13:22:07 +0800

    完成gRPC.netframework的案例。

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Grpc.Core;
+using GRPCDemo;
+
+namespace gRPCClient
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:9007",ChannelCredentials.Insecure);
+            
+            var client = new gRPC.gRPCClient(channel);
+            var reply = client.SayHello(new HelloRequest {Name = "LineZero"});
+            Console.WriteLine("来自" + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPCDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("gRPCDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gRPCDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("43239A6A-5B6F-4D61-B503-AE5E89092974")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
