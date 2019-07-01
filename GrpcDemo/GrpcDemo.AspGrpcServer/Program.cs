80dcd21cc70635ccd2352bac7f98d7b8391c1189
daihb <daihb@DESKTOP-R31MQAB>
2019-07-01 08:55:33 +0800

    增加asp.net 2.2里使用rpc。

---
+++ GrpcDemo/GrpcDemo.AspGrpcServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using GrpcDemo.AspGrpcServer.Services;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace GrpcDemo.AspGrpcServer
+{
+    public class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            Grpc.Core.Server server = new Grpc.Core.Server
+            {
+                Services =
+                {
+                    Greeter.BindService(new GreeterService())
+                },
+                Ports = { new Grpc.Core.ServerPort("localhost", Port, Grpc.Core.ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("gRPC服务开启的端口 " + Port);
+            Console.WriteLine("任意键退出");
+            Console.ReadKey();
+
+            host.Run();
+
+            server.ShutdownAsync().Wait();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ GrpcDemo/GrpcDemo.AspGrpcServer/Services/GreeterService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcDemo.AspGrpcServer.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = $"你好，{request.Name},这里是asp.net server" });
+        }
+    }
+}
