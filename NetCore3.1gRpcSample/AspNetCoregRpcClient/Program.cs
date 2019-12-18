55309cb24e6c76c9d375c9128941b5742c341556
daihb <daihb@DESKTOP-R31MQAB>
2019-12-18 17:17:23 +0800

    增加双向流案例

--- NetCore3.1gRpcSample/AspNetCoregRpcClient/Program.cs
+++ NetCore3.1gRpcSample/AspNetCoregRpcClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoregRpcService;
+using Grpc.Core;
 using Grpc.Net.Client;
 using System;
 using System.Threading.Tasks;
@@ -10,13 +11,47 @@ namespace AspNetCoregRpcClient
         static async Task Main(string[] args)
         {
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Michael" });
-            Console.WriteLine("Greeter 服务返回数据：" + reply.Message);
+
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(new HelloRequest { Name = "Michael" });
+            //Console.WriteLine("Greeter 服务返回数据：" + reply.Message);
 
             var catClient = new LuCat.LuCatClient(channel);
-            var catReply = await catClient.SuckingCatAsync(new Google.Protobuf.WellKnownTypes.Empty());
-            Console.WriteLine("调用撸猫服务：" + catReply.Message);
+            //var catReply = await catClient.SuckingCatAsync(new Google.Protobuf.WellKnownTypes.Empty());
+            //Console.WriteLine("调用撸猫服务：" + catReply.Message);
+
+            var catCount = await catClient.CountAsync(new Google.Protobuf.WellKnownTypes.Empty());
+            Console.WriteLine($"一共{catCount.Count}只猫。");
+            var rand = new Random(DateTime.Now.Millisecond);
+
+            var bathCat = catClient.BathTheCat();
+            // 定义接收吸猫响应逻辑
+            var bathCatRespTask = Task.Run(async () => 
+            {
+                await foreach (var resp in bathCat.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(resp.Message);
+                }
+            });
+
+            // 随机给10个猫洗澡
+            for (int i = 0;i < 10;i++)
+            {
+                await bathCat.RequestStream.WriteAsync(new BathTheCatReq
+                {
+                    Id = rand.Next(0,catCount.Count)
+                });
+            }
+
+            // 发送完毕
+            await bathCat.RequestStream.CompleteAsync();
+            Console.WriteLine("客户端已发送完10个需要洗澡的猫id");
+            Console.WriteLine("接收洗澡结果！");
+
+            // 开始接收响应
+            await bathCatRespTask;
+
+            Console.WriteLine("洗澡完毕");
         }
     }
 }
--- NetCore3.1gRpcSample/AspNetCoregRpcService/Services/LuCatService.cs
+++ NetCore3.1gRpcSample/AspNetCoregRpcService/Services/LuCatService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoregRpcService.Services
 {
@@ -19,6 +20,44 @@ namespace AspNetCoregRpcService.Services
         };
 
         private static readonly Random Rand = new Random(DateTime.Now.Millisecond);
+
+        private readonly ILogger<LuCatService> _logger;
+
+        public LuCatService(ILogger<LuCatService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task BathTheCat(IAsyncStreamReader<BathTheCatReq> requestStream, 
+            IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
+        {
+            var bathQueue = new Queue<int>();
+
+            while (await requestStream.MoveNext())
+            {
+                // 将要洗澡的猫加入队列
+                bathQueue.Enqueue(requestStream.Current.Id);
+                _logger.LogInformation($"Cat {requestStream.Current.Id} Enqueue.");
+            }
+
+            // 遍历队列开始洗澡
+            while (bathQueue.TryDequeue(out var catId))
+            {
+                await responseStream.WriteAsync(new BathTheCatResp
+                {
+                    Message = $"成功给一只{Cats[catId]}洗了澡！"
+                });
+                await Task.Delay(500);
+            }
+        }
+
+        public override Task<CountCatResult> Count(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new CountCatResult 
+            { 
+                Count = Cats.Count
+            });
+        }
 
         public override Task<SuckingCatResult> SuckingCat(Empty request,ServerCallContext context)
         {
